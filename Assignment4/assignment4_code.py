e08c2d4057fbcd21844e6fc536d210b3ad0d87dc
Pablo Veyrat <veyratpablo@gmail.com>
2021-01-27 09:59:36 +0100

    changes to HW3 and 4

--- Assignment4/assignment4_code.py
+++ Assignment4/assignment4_code.py
@@ -14,6 +14,9 @@ from rl.gen_utils.common_funcs import get_logistic_func, get_unit_sigmoid_func
 import matplotlib.pyplot as plt
 import time
 from Assignment3.assignment3_code import *
+from scipy.stats import poisson
+from rl.chapter3.simple_inventory_mdp_cap import SimpleInventoryMDPCap,InventoryState
+
 
 #PROBLEM 2
 def process_time(n,gamma = 1) -> Tuple[float,float,float]:
@@ -90,8 +93,88 @@ class Problem3(NamedTuple):
             else:
                 list_actions.append("D")
         return list_actions
-        
+
+
+#PROBLEM 4
+#We represent actions as a tuple of store 1 supplier order, store 2 supplier order and
+#transfer between both stores
+
+#We define a custom state class for this problem
+@dataclass(frozen=True)
+class State:
+    state1:InventoryState
+    state2:InventoryState
+
+Problem4Mapping = StateActionMapping[State,Tuple[int,int,int]]
+
+class ComplexMDP(FiniteMarkovDecisionProcess[State, Tuple[int,int,int]]):
     
+    def __init__(
+        self,
+        store1: SimpleInventoryMDPCap,
+        store2: SimpleInventoryMDPCap,
+        K1: float,
+        K2: float
+        ):
+        self.store1: SimpleInventoryMDPCap = store1
+        self.store2: SimpleInventoryMDPCap = store2
+        self.K1 = K1
+        self.K2 = K2
+
+        super().__init__(self.get_action_transition_reward_map())
+    
+    def get_action_transition_reward_map(self) -> Problem4Mapping:
+        d: Dict[State, Dict[Tuple[int,int,int], Categorical[Tuple[State,
+                                                            float]]]] = {}
+
+        for state1 in store1.states():
+            for state2 in store2.states():
+                state = State(state1 = state1,state2 = state2)
+                d1 : Dict[Tuple[int,int,int], Categorical[Tuple[State,
+                                                                float]]] = {}
+                state1_actions = store1.actions(state1)
+                state2_actions = store2.actions(state2)
+                list_actions: Sequence[Tuple[int,int,int]] = []
+                for k in state1_actions:
+                    for l in state2_actions:
+                        list_actions+=[(k,l,0)]
+                        #We introduce extra-actions corresponding to transfers between stores
+                        for u in range(0,k):
+                            if u<=state2.on_hand:
+                                list_actions+=[(k-u,l,u)]
+                        for u in range(0,l):
+                            if u<=state1.on_hand:
+                                list_actions+=[(k,l-u,-u)]
+                for j in list_actions:
+                    sr_probs_dict: Dict[Tuple[State, float], float] = {}
+                    action1,action2,transfer = j[0],j[1],j[2]
+                    #Transfers directly affect on_hand inventory
+                    new_state1 = InventoryState(on_hand = state1.on_hand+transfer,on_order = state1.on_order)
+                    new_state2 = InventoryState(on_hand = state2.on_hand-transfer,on_order = state2.on_order)
+                    mapping1 = store1.mapping[new_state1][action1]
+                    mapping2 = store2.mapping[new_state2][action2]
+                    for k in mapping1:
+                        for l in mapping2:
+
+                            new_state = State(state1 = k[0][0],state2 = l[0][0])
+                            reward = k[0][1]+l[0][1]
+                            if action1>0:
+                                reward -= self.K1
+                            if action2>0:
+                                reward -= self.K1
+                            if transfer!=0:
+                                reward -= self.K2
+                            proba = k[1]*l[1]
+                            sr_probs_dict[(new_state,reward)] = proba
+                    try:
+                        d1[j] = Categorical(sr_probs_dict)
+                    except:
+                        print("Error")
+                        pass
+                d[state] = d1
+        return d
+
+
 if __name__ == '__main__':
     #PROBLEM 2
     """
@@ -108,7 +191,7 @@ if __name__ == '__main__':
         list_value.append(b)
         list_policy.append(c)
     plot_time(list_brute,list_value,list_policy,max_N)
-    """
+
     #PROBLEM 3
     print("Solving Problem 3")    
     gamma:float = 0.8
@@ -128,6 +211,43 @@ if __name__ == '__main__':
     
     print(optimal_value_function)
     print(optimal_policy)
+    """
+    print("Solving Problem 4")
+    user_capacity = 2
+    user_poisson_lambda = 1.0
+    user_holding_cost = 1.0
+    user_stockout_cost = 10.0
+    user_gamma = 0.9
+    
+    user_capacity2 = 3
+    user_poisson_lambda2 = 0.9
+    user_holding_cost2 = 1.5
+    user_stockout_cost2 = 15.0
+
+    store1: FiniteMarkovDecisionProcess[InventoryState, int] =\
+        SimpleInventoryMDPCap(
+            capacity=user_capacity,
+            poisson_lambda=user_poisson_lambda,
+            holding_cost=user_holding_cost,
+            stockout_cost=user_stockout_cost
+        ) 
+    store2: FiniteMarkovDecisionProcess[InventoryState, int] =\
+        SimpleInventoryMDPCap(
+            capacity=user_capacity2,
+            poisson_lambda=user_poisson_lambda2,
+            holding_cost=user_holding_cost2,
+            stockout_cost=user_stockout_cost2
+        )
+    K1 = 1
+    K2 = 1
+    problem4 = ComplexMDP(store1 = store1,
+                          store2 = store2,
+                          K1 = K1,
+                          K2 = K2
+                          )
+    value_opt = value_iteration_result(problem4,user_gamma)
+    policy_opt = policy_iteration_result(problem4,user_gamma)
+    
     
     
     
